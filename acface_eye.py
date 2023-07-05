ea078d68e06d0b2f2bf2b8f21559faafe2695dc2
Jayson Jeganathan <jayson.jeganathan@gmail.com>
2023-07-05 10:38:22 +1000

    acface_eye

---
+++ acface_eye.py
@@ -0,0 +1,6 @@
+import numpy as np, pandas as pd, matplotlib.pyplot as plt
+import acommonfuncs, acface_utils
+from acface_utils import target_fps, ntrials, n_trialsperemotion, emots
+from acommonvars import *
+
+c = acommonfuncs.clock()
