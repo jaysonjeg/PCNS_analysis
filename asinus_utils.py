7803668491a7474241c8324220b6200a7686ed07
Jayson Jeganathan <jayson.jeganathan@gmail.com>
2023-07-11 21:09:10 +1000

    updated asinus

---
+++ asinus_utils.py
@@ -0,0 +1,32 @@
+import numpy as np, pandas as pd
+from scipy import signal
+
+time_interval = 0.04 #seconds per frame
+fs = int(1/time_interval) #sampling frequency
+
+def lowpass(data,lowcut,axis=-1):
+    order = 10
+    b, a = signal.butter(order, lowcut, btype='low', fs=fs, output='ba')
+    y = signal.lfilter(b, a, data,axis=axis)
+    return y    
+
+def PLV(x,y):
+    #calculate phase locking value of vectors x and y
+    e = np.exp(1j * (x - y))
+    return np.abs(np.sum(e)) / len(e)
+
+def get_peak_heights(ts):
+    #Given a time series, return the heights of all peaks. Peaks should be separated by 30 frames
+    peaks,properties = signal.find_peaks(ts, distance=30)
+    return ts[peaks]
+
+def get_grad_peak_heights(ausn_smoo):
+    """
+    Returns a list of peak heights of the gradient of the time series in ausn
+    ausn is 3D array of ntrials * nframes * nAUs
+    lowpass_cutoff is the lowpass cutoff in Hz. try lowpass_cutoff 1 (heavy smoothing), 2 or 4 (light smoothing)
+    """
+    ausn_smoo_grad = np.gradient(ausn_smoo,axis=1)
+    ausn_smoo_grad_peaks = [get_peak_heights(ausn_smoo_grad[i,:]) for i in range(ausn_smoo_grad.shape[0])]
+    ausn_smoo_grad_peaks = np.hstack(ausn_smoo_grad_peaks)
+    return ausn_smoo_grad_peaks
